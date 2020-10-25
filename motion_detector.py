c20990919b61b070ff8a4948458c29f56a68fb2e
Velidonimo <velidon@mail.ru>
2020-10-25 14:23:28 +0300

    motion_detecting_done

--- motion_detector.py
+++ motion_detector.py
@@ -15,7 +15,7 @@ while True:
     # storing the first frame to compare the current frame with it
     # waiting a couple frames to warm the camera up
     if comparing_frame is None:
-        if frame_counter > 20:
+        if frame_counter > 50:
             comparing_frame = gray_frame
             continue
         else:
@@ -27,9 +27,18 @@ while True:
     # smoothing image a little by delating the white fields
     thresh_delta_frame = cv2.dilate(thresh_delta_frame, None, iterations=2)
 
-    # cv2.imshow('Current frame', gray_frame)
+    # finding contours of white areas bigger then 1000px
+    (cntrs, _) = cv2.findContours(thresh_delta_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in cntrs:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x,y,w,h) = cv2.boundingRect(contour)
+        cv2.rectangle(current_frame, (x,y), (x+w, y+h), (0,255,0), 3)
+
+
+    cv2.imshow('Current frame', current_frame)
     # cv2.imshow('Comparing', comparing_frame)
-    cv2.imshow('Delta', thresh_delta_frame)
+    #cv2.imshow('Delta', thresh_delta_frame)
 
     key = cv2.waitKey(1)
     if key == ord("q"):
